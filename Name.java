e851732865d9597eefed88297eeac67f3ff20fd1
JeffChapman <jeffchapman@cmail.carleton.ca>
2013-10-23 17:19:29 -0400

    Update Name.java

--- Name.java
+++ Name.java
@@ -2,22 +2,41 @@ public class Name
 {
    private String first, last;
    public Name(String first, String last) {
-	   this.first = first;
-	   this.last = last;
+           this.first = first;
+           this.last = last;
    }
    
    public String getFirst() {
-	   return first;
+           return first;
    }
    
    public String getLast() {
-	   return last;
+           return last;
    }
    
    public String toString() { 
-	   return first + " " + last;
+           return first + " " + last;
    }
    public boolean equals(Object o) {
-	   return false;
+	   if(o instanceof Name)
+	   {
+		   Name b = (Name)o;
+		   if(b.getFirst().equals(first))
+		   {
+			   if(b.getLast().equals(last))
+			   {
+				   return true;
+			   }else
+			   {
+				   return false;
+			   }
+		   }else
+		   {
+			   return false;
+		   }
+	   }else
+	   {
+		   return false;
+	   }
    }
 }
