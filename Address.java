4e0856b0431469c75d4c143b670afb96ddc7f978
chrissparrow <chrissparrow@cmail.carleton.ca>
2013-10-23 17:29:49 -0400

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,3 +1,4 @@
+
 public class Address
 {
     private int unit;
@@ -5,14 +6,39 @@ public class Address
     private String city;
     private String postalCode;
 
-    public Address (String street, String city, String postalCode) throws Exception
-    { };
-    public Address (int unit, String street, String city, String postalCode) throws Exception
-    { };
+    public Address (String street, String city, String postalCode) throws Exception{
+    	if (!isValidPostalCode(postalCode)) {
+    		throw new Exception("Invalid Postal Code");
+    	}
+    	this.unit = 0;
+    	this.street = street;
+    	this.city = city;
+    	this.postalCode = postalCode;
+    }
+    
+    public Address (int unit, String street, String city, String postalCode) throws Exception {
+    	if (unit < 1) {
+    		throw new Exception("Cannot have a unit number below 1");
+    	}
+    	if (!isValidPostalCode(postalCode)) {
+    		throw new Exception("Invalid Postal Code");
+    	}
+    	this.unit = unit;
+    	this.street = street;
+    	this.city = city;
+    	this.postalCode = postalCode;
+    }
 
-     public String toString() { return null;}
+     public String toString() { 
+    	 if (unit == 0) {
+    		 return street + ", " + city + " " + postalCode;
+    	 }
+    	 return unit + " " + street + ", " + city + " " + postalCode;
+     }
      public boolean equals(Object o) { return false; }
      
-     private boolean isValidPostalCode( String postalCode ) { return false; }
+     private boolean isValidPostalCode( String postalCode ) { 
+    	 return this.postalCode.length() == 6;
+     }
  
 } 
