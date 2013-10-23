dbd76108cb531eb04f1ccfda5dd107f9528518a6
chrissparrow <chrissparrow@cmail.carleton.ca>
2013-10-23 17:00:21 -0400

    Update Professor.java
    Done

--- Professor.java
+++ Professor.java
@@ -3,11 +3,17 @@ public class Professor extends Person
 {
    private String office;
 
-   public Professor(String firstName, String lastName) {
+   public Professor(String firstName, String lastName) { 
 	   super(firstName, lastName);
+	   this.office = null;
    }
-   public void setOffice(String office) { }
-   public String getOffice() { return null;}
-
-   // You decide if you need toString() and equals()
+   
+   public void setOffice(String office) {
+	   this.office = office;
+   }
+   public String getOffice() {
+	   return this.office;
+   }
+   
 }
+
