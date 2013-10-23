048f00ca136417cc22ff0bc8ba603f76bbea53bd
chrissparrow <chrissparrow@cmail.carleton.ca>
2013-10-23 16:36:57 -0400

    Update Person.java
    Done except equals

--- Person.java
+++ Person.java
@@ -1,25 +1,52 @@
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 public class Person
 {
    private Name name;
    private Address address;
    private ArrayList<Course> courses;
    
-   public Person( String first, String last) {};
+   public Person( String first, String last) {
+	   this.name = new Name(first, last);
+	   this.address = null;
+	   this.courses = null;
+   }
    
-   public void setLastName( String name) {};
-   public String getLastName() { return null; };
-   public String getFirstName() { return null; };
-   public String getFullName() { return null; };
+   public void setLastName( String name) {
+	   this.name = new Name(this.name.getFirst(), name);
+   }
+   
+   public String getLastName() {
+	   return this.name.getLast();
+   }
+   
+   public String getFirstName() { 
+	   return this.name.getFirst();
+   }
+   public String getFullName() { 
+	   return this.name.toString();
+   }
 
-   public void setAddress(Address address) {};
-   public Address getAddress() { return null; };
+   public void setAddress(Address address) {
+	   this.address = address;
+   }
+   public Address getAddress() { 
+	   return this.address;
+   }
 
-   public Course[] getCourses() { return null; }
-   public void addCourse( Course course ) {};
-   public void remove(Course course) {};
+   public Course[] getCourses() { 
+	   return (Course[])this.courses.toArray();
+   }
+   public void addCourse( Course course ) {
+	   this.courses.add(course);
+   }
+   public void remove(Course course) {
+	   this.courses.remove(course);
+   }
 
-    public String toString() { return null; }
+    public String toString() { 
+    	return "Name: " + this.name.toString() + " Address: " + this.address.toString() + " Courses: " + courses.toString(); }
+    
     public boolean equals(Object o) { return false; }
 
-}    
+}     
