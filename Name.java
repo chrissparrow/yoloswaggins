192be27eb5789f33f5e3ef7fa469394a52b7c964
chrissparrow <chrissparrow@cmail.carleton.ca>
2013-10-23 16:36:13 -0400

    Update Name.java
    Done

--- Name.java
+++ Name.java
@@ -1,7 +1,23 @@
 public class Name
 {
    private String first, last;
-   public Name(String first, String last) {}
-   public String toString() { return null; }
-   public boolean equals(Object o) { return false; }
+   public Name(String first, String last) {
+	   this.first = first;
+	   this.last = last;
+   }
+   
+   public String getFirst() {
+	   return first;
+   }
+   
+   public String getLast() {
+	   return last;
+   }
+   
+   public String toString() { 
+	   return first + " " + last;
+   }
+   public boolean equals(Object o) {
+	   return false;
+   }
 }
